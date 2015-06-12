a25bf2f7fcd38d3e59ee021c435a30f9865f3079
cclauss <cclauss@bluewin.ch>
2015-06-12 05:13:32 +0200

    Update temperature_gui.py

--- temperature_gui.py
+++ temperature_gui.py
@@ -1,46 +1,45 @@
 # coding: utf-8
 
 # http://omz-forums.appspot.com/pythonista/post/5809271396630528
-# calculations are incorrect!!!
+# calculations are NOT correct!!!
 
 import ui
 
-w, h = ui.get_screen_size()
+view = ui.View(name='Temperature Converter')
+view.hidden = True
+view.present()
+_, _, w, h = view.bounds
 
 text_view = ui.TextView()
 text_view.alignment = ui.ALIGN_CENTER
 text_view.center = (w/2, h/4)
 
 button = ui.Button(title='Convert')
-button.border_width = 2
 button.center = (w/2, h/2)
 
 label = ui.Label()
-label.width = w
+label.width = w/2
 label.alignment = ui.ALIGN_CENTER
-label.center = (w/2, 3*h/4)
+label.background_color = 'white'
+label.center = (w/2, h*3/4)
 label.number_of_lines = 4
-label.text = '''Enter a temperature above and tap "Convert".
+label.text = 'Enter a temperature above and tap "Convert".'
 
-Swipe down with two fingers to quit the app.'''
-
-view = ui.View(title='Temperature Converter')
 view.add_subview(text_view)
 view.add_subview(button)
 view.add_subview(label)
-view.present(hide_title_bar=True)
+view.hidden = False
 
-fmt = '''
-{:g} degrees Fahrenheit is {:g} degrees Celsius.
+fmt = '''{:g} degrees Fahrenheit is {:g} degrees Celsius.
 
 {:g} degrees Celsius is {:g} degrees Fahrenheit.'''
 
 def convert_action(sender):
     try:
-        value = float(text_view.text)
+        value = float(text_view.text.strip())
     except ValueError:
         value = 50
-        text_view.text = str(value)
+    text_view.text = '{:g}'.format(value)
     label.text = fmt.format(value, value * 1.2, value, value * 0.8)
 
 button.action = convert_action
