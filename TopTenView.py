9ffdb3017bbc0a6f3d1f7d4ce0bd2376832f400f
cclauss <cclauss@bluewin.ch>
2015-09-30 13:38:26 +0200

    Move to buttons

--- TopTenView.py
+++ TopTenView.py
@@ -1,24 +1,26 @@
-# ui View for the top ten iTunes songs.
-# This code only seems to run half the time.  Pull requests gladly accepted.
-
 # coding: utf-8
 
-import feedparser, ui
+# ui.View subclass for the top ten iTunes songs.
+# Pull requests gladly accepted.
+
+import feedparser, requests, ui
 
 url = 'https://itunes.apple.com/us/rss/topsongs/limit=10/xml'
 
 def get_image_urls(itunes_url):
     for entry in feedparser.parse(itunes_url).entries:
-         yield entry['summary'].partition('src="')[2].partition('"')[0]
+        yield entry['summary'].partition('src="')[2].partition('"')[0]
 
 class TopTenView(ui.View):
     def __init__(self, image_urls):
         self.present()
         for i, url in enumerate(image_urls):
-            image_view = ui.ImageView()
-            image_view.load_from_url(url)
-            self.add_subview(image_view)
-            image_view.x = (i % 5) * 128 + 10
-            image_view.y = (i / 5) * 128 + 10
+            button = ui.Button()
+            button.background_image = ui.Image.from_data(requests.get(url).content)
+            button.border_width = 2
+            button.x = (i % 5) * 128 + 10
+            button.y = (i / 5) * 128 + 10
+            button.width = button.height = 128
+            self.add_subview(button)
 
-TopTenView(get_image_urls(url))
+TopTenView(list(get_image_urls(url)))
